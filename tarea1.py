79e730b256dde7cc5f00daebba434788df0e0a28
RafaSevil <rafaintriago7@gmail.com>
2024-08-24 17:45:03 -0500

    Update and rename prueba.py to tarea1.py

---
+++ tarea1.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "GG": "Good game (Buena partida)",
+            "AFK": "Away from keyboard (Lejos del teclado)",
+            "BTW": "By the way (Por cierto)",
+            "CLICKBAIT": "Contenido diseñado para atraer clics de manera engañosa.",
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ").upper()
+if word in meme_dict.keys():
+    print(meme_dict[word])
+    
+else:("Esta palabra no esta en el diccionario")
+   
